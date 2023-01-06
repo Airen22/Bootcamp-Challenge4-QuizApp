05e98f63e3cb922407c718aa6b6d01237b409677
Erin <erinpeifer@gmail.com>
2023-01-05 19:18:28 -0600

    added validation, corrected question rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,15 @@ var timerElement = document.querySelector(".timer-count");
 var startPage = document.querySelector(".start-page");
 var questionContainer = document.querySelector(".question-container");
 var trivia = document.querySelector("#question");
-var answerA = document.querySelector("#answer-a");
-var answerB = document.querySelector("#answer-b");
-var answerC = document.querySelector("#answer-c");
+var btns = document.querySelector("#btns");
+// var answerA = document.querySelector("#answer-a");
+// var answerB = document.querySelector("#answer-b");
+// var answerC = document.querySelector("#answer-c");
 var hide = document.querySelector(".hide");
 var score = 0
 var timer;
 var timerCount;
+var currentQuestion = 0;
 
 
 function startGame() {
@@ -31,63 +33,73 @@ function startTimer() {
 var quizQuestions = [
     {
         question: "How long is the olympic swimming pool?",
-        answers: {
-          a: "20m",
-          b: "50m",
-          c: "100m",
-        },
-        correct: 'c'
+        answers: ["20m","50m","100m"],
+        correct: "50m"
     },
     {
       question: "What sport involves the Stanley Cup?",
-      answers: {
-        a: "Golf",
-        b: "Hockey",
-        c: "NASCAR",
-      },
-      correct: 'b'
+      answers: ["Golf","Hockey", "NASCAR"],
+      correct: "Hockey"
     },
     {
       question: "Which location refers to the deepest point in the ocean?",
-      answers: {
-        a: "Mariana's Point",
-        b: "Oceanic Limit",
-        c: "Challenger Deep",
-      },
-      correct: 'c'
+      answers: ["Mariana's Point", "Oceanic Limit", "Challenger Deep"],
+      correct: "Mariana's Point",
     },
 ]
 
 function renderQuestions() {
+  var questionIndex = quizQuestions[currentQuestion];
+  console.log(questionIndex);
   startPage.style.display = 'none';
   hide.style.display = 'block';
-  for (var i = 0; i < quizQuestions.length; i++) {
-    trivia.textContent = quizQuestions[i].question;
-    answerA.textContent = quizQuestions[i].answers.a;
-    answerB.textContent = quizQuestions[i].answers.b;
-    answerC.textContent = quizQuestions[i].answers.c;
-    
-    answerA.addEventListener("click", checkA)
-    // if (quizQuestions[i].correct === 'a') {
-    //     return;
-    //   } else {
-    //     timerCount-10
-    //   }
-    
- 
+
+  trivia.textContent = questionIndex.question;
+  console.log(questionIndex.question);
+  btns.innerHTML = "";
+  questionIndex.answers.forEach(answer => {
+    var button = document.createElement("button");
+    button.setAttribute("class", "btn")
+    button.setAttribute("value", answer)
+    button.textContent = answer;
+    button.onclick = checkAnswer;
+    btns.appendChild(button);
+      })
+
     }
+
+    var checkAnswer = function() {
+      if (this.value !== quizQuestions[currentQuestion].correct) {
+        timerCount -=10;
+        timerElement.textContent = "Timer: " + timerCount;
+        // message "incorrect"
+      } else {
+        // message "correct"
+      } 
+currentQuestion++;
+      if (currentQuestion === quizQuestions.length) {
+        endQuiz()
+      } else {
+        renderQuestions()
+      }
+      
+      // renderQuestions();
+    }
+    
+
+
     // console.log(quizQuestions[i].answers.a);
     // console.log(quizQuestions[i].answers.b);
     // console.log(quizQuestions[i].answers.c);
-  }
+  
 
-  function checkA() {
-    if (quizQuestions[i].correct === 'a') {
-      return;
-    } else {
-      timerCount-10
-    }
-  }
+  // function checkA() {
+  //   if (quizQuestions[0].correct === 'a') {
+  //     document.getElementById(".message")="correct";
+  //   } else {
+  //     timerCount-10
+  //   }
+  // }
 
     // div.innerHTML += 'button'
     // var obj = quizQuestions[i];
@@ -113,6 +125,7 @@ function renderQuestions() {
 // for (var i = 0; i < quizQuestions; i++) {
 //   quizQuestions.textContent = question + answers
 // }
+
 
 
 
